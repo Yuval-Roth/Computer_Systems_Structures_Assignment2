9007dd1409c267d8eb8af871466e39e2dd39f8c0
Yuval <rothyuv@post.bgu.ac.il>
2022-12-22 22:47:23 +0200

    Finished until part 7

--- Machine/CPU16.cs
+++ Machine/CPU16.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SimpleComponents;
+﻿using SimpleComponents;
+using System;
 
 namespace Machine
 {
-    public class CPU16 
+    public class CPU16
     {
-         public const int J0 = 0, J1 = 1, J2 = 2, D0 = 3, D1 = 4, D2 = 5, C0 = 6, C1 = 7, C2 = 8, C3 = 9, C4 = 10, A = 11, X0 = 12, X1 = 13, X2 = 14, Type = 15;
+        public const int 
+            J0 =  0,
+            J1 =  1,
+            J2 =  2,
+            D0 =  3,
+            D1 =  4,
+            D2 =  5,
+            C0 =  6,
+            C1 =  7,
+            C2 =  8,
+            C3 =  9,
+            C4 = 10,
+            A  = 11,
+            X0 = 12,
+            X1 = 13,
+            X2 = 14,
+            Type = 15;
 
         public int Size { get; private set; }
 
@@ -30,7 +42,7 @@ namespace Machine
 
         public CPU16()
         {
-            Size =  16;
+            Size = 16;
 
             Instruction = new WireSet(Size);
             MemoryInput = new WireSet(Size);
@@ -71,25 +83,64 @@ namespace Machine
             ConnectControls();
         }
 
-       //Add gates for control implementation here
+        //Add gates for control implementation here
 
+        BitwiseMux A_Input_Mux;
+        BitwiseMux A_Or_M_Mux;
+        WireSet ALU_Control;
+        AndGate D_Load_AndGate;
+        OrGate A_Load_OrGate;
+        AndGate A_Load_AndGate;
+        NotGate A_Load_NotGate;
+        AndGate MemoryWrite_AndGate;
 
         private void ConnectControls()
         {
             //1. connect control of mux 1 (selects entrance to register A)
+            A_Input_Mux = new BitwiseMux(Size);
+            A_Input_Mux.ConnectInput1(m_gALU.Output);
+            A_Input_Mux.ConnectInput2(Instruction);
 
             //2. connect control to mux 2 (selects A or M entrance to the ALU)
-
+            A_Or_M_Mux = new BitwiseMux(Size);
+            A_Or_M_Mux.ConnectInput1(MemoryOutput);
+            A_Or_M_Mux.ConnectInput2(A_Input_Mux.Output);
 
             //3. consider all instruction bits only if C type instruction (MSB of instruction is 1)
+            A_Input_Mux.Control.ConnectInput(Instruction[Type]);
+            A_Or_M_Mux.Control.ConnectInput(Instruction[A]);
 
             //4. connect ALU control bits
+            ALU_Control = new WireSet(5);
+            ALU_Control[0].ConnectInput(Instruction[C0]);
+            ALU_Control[1].ConnectInput(Instruction[C1]);
+            ALU_Control[2].ConnectInput(Instruction[C2]);
+            ALU_Control[3].ConnectInput(Instruction[C3]);
+            ALU_Control[4].ConnectInput(Instruction[C4]);
+            m_gALU.Control.ConnectInput(ALU_Control);
 
             //5. connect control to register D (very simple)
+            D_Load_AndGate = new AndGate();
+            D_Load_AndGate.ConnectInput1(Instruction[D1]);
+            D_Load_AndGate.ConnectInput2(Instruction[Type]);
+            m_rD.Load.ConnectInput(D_Load_AndGate.Output);
 
             //6. connect control to register A (a bit more complicated)
+            A_Load_OrGate = new OrGate();
+            A_Load_AndGate = new AndGate();
+            A_Load_NotGate = new NotGate();
+            A_Load_NotGate.ConnectInput(Instruction[Type]);
+            A_Load_AndGate.ConnectInput1(Instruction[Type]);
+            A_Load_AndGate.ConnectInput2(Instruction[D2]); 
+            A_Load_OrGate.ConnectInput2(A_Load_NotGate.Output);
+            A_Load_OrGate.ConnectInput1(A_Load_AndGate.Output);
+            m_rA.Load.ConnectInput(A_Load_OrGate.Output);
 
             //7. connect control to MemoryWrite
+            MemoryWrite_AndGate = new AndGate();
+            MemoryWrite_AndGate.ConnectInput1(Instruction[D0]);
+            MemoryWrite_AndGate.ConnectInput2(Instruction[Type]);
+            MemoryWrite.ConnectInput(MemoryWrite_AndGate.Output);
 
             //8. create inputs for jump mux
 
@@ -97,7 +148,7 @@ namespace Machine
 
             //10. connect PC load control
         }
-        
+
 
         public override string ToString()
         {
